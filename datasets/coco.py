c09ef6d0a6df94ad037217231be5955d2891c7c9
VimalVkMcW <venkatachalam.kannappan@multicorewareinc.com>
2024-03-21 11:52:23 +0530

    commit

--- datasets/coco.py
+++ datasets/coco.py
@@ -150,7 +150,7 @@ def build(image_set, args):
     mode = 'instances'
     PATHS = {
         "train": (root / "train2017", root / "annotations/annotations" / f'{mode}_train2017.json'),
-        "val": (root / "val2017", root / "annotations/annotations" / f'{mode}_val2017.json'),
+        "val": (root / "val2017/val2017", root / "annotations/annotations" / f'{mode}_val2017.json'),
     }
 
     img_folder, ann_file = PATHS[image_set]
